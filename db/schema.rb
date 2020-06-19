7c566bb69e61d6971f0bd74c2e49911ca0e59384
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-06-19 17:05:08 +0300

    install app gems dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_19_130531) do
+
+  create_table "tweeets", force: :cascade do |t|
+    t.text "tweeet"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
